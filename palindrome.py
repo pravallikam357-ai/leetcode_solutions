82733878948d9c18f208fd76e66ff755a60ace59
pravallikam357-ai <pravallikam357@gmail.com>
2026-05-02 20:26:17 +0530

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,4 +1,17 @@
-import math
+# Problem: Palindrome
+# Platform: LeetCode
+# Difficulty: Easy
+# Topic: Math
+# Language: Python
+
+#  Problem Description:
+# To find the given integer is palindrome or not
+
+#  Approach:
+# Converting the number into string and reverse it and compare.
+
+# ⚡ Time Complexity: O(n)
+# ⚡ Space Complexity: O(n)import math
 class Solution(object):
     def isPalindrome(self, x):
         y=str(x)
