2248c4c8c93bddd0b077c7600edc4b5c8439ff13
pravallikam357-ai <pravallikam357@gmail.com>
2026-05-02 20:16:16 +0530

    Update two_sum.py

--- twosumleet.py
+++ twosumleet.py
@@ -1,4 +1,17 @@
-class Solution(object):
+# Problem: Two Sum
+# Platform: LeetCode
+# Difficulty: Easy
+# Topic: Arrays
+# Language: Python
+
+#  Problem Description:
+# Find two numbers that add up to a target.
+
+#  Approach:
+# Use hashmap to store values and check complement.
+
+# ⚡ Time Complexity: O(n)
+# ⚡ Space Complexity: O(n)class Solution(object):
     def twoSum(self, nums, target):
         for i in range(len(nums)):
             for j in range(i+1,len(nums)):
