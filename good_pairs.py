3e0c27df15f4c33b2cb56611d7ef364066a67d71
pravallikam357-ai <pravallikam357@gmail.com>
2026-05-03 19:14:36 +0530

    Update good_pairs.py

--- good_pairs.py
+++ good_pairs.py
@@ -1,3 +1,20 @@
+# Problem: Number of Good pairs
+# Platform: LeetCode
+# Difficulty: Easy
+# Topic: Arrays,Hashtables,Math
+# Language: Python
+
+#  Problem Description:
+# Take the two lists and compare every two values if equal increment the count by one.
+
+#  Approach:
+# You use two nested loops:
+
+#Outer loop picks each element i
+#Inner loop checks all elements after it (j = i+1)
+
+#  Time Complexity: O(n²)
+#  Space Complexity: O(1)
 class Solution(object):
     def numIdenticalPairs(self, nums):
         count=0
