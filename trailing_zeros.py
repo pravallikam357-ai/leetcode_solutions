be3cbea9daa92be70fddadc663705a85b32c03ae
pravallikam357-ai <pravallikam357@gmail.com>
2026-05-04 20:52:28 +0530

    Update trailing_zeros.py

--- trailing_zeros.py
+++ trailing_zeros.py
@@ -1,3 +1,17 @@
+# Problem: Factorial Trailing zeros
+# Platform: LeetCode
+# Difficulty: Medium
+# Topic: Math
+# Language: Python
+
+#  Problem Description:
+# Find the number of trailing zeros in a factorial.
+
+#  Approach:
+# Find the number of 5's in factrs of the given number.
+
+#  Time Complexity: O(logn)
+#  Space Complexity: O(1)
 class Solution(object):
     def trailingZeroes(self, n):
         count=0
