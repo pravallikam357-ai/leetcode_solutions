83b6783cce0415bb469a57255716c40204cc6d93
pravallikam357-ai <pravallikam357@gmail.com>
2026-05-05 20:14:03 +0530

    Add files via upload

---
+++ ugly_number.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def isUgly(self, n):
+        while n>1:
+            if n%2==0:
+                n=n//2
+            elif n%3==0:
+                n=n//3
+            elif n%5==0:
+                n=n//5
+            else:
+                return False
+        if n<=0:
+            return False
+        return True
+        
