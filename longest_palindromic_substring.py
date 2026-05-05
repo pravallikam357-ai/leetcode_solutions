cef9d42cd1119debd7d6d85c8f48ee7a4570e901
pravallikam357-ai <pravallikam357@gmail.com>
2026-05-05 20:12:28 +0530

    Update longest_palindromic_substring.py

--- longest_palindromic_substring.py
+++ longest_palindromic_substring.py
@@ -1,3 +1,17 @@
+# Problem: Longest palindromic substring
+# Platform: LeetCode
+# Difficulty: Medium
+# Topic: Two pointers,strings,dynamic programming
+# Language: Python
+
+#  Problem Description:
+# Find the longest palindromic substring in a given string.
+
+# Approach:
+# Obtain all substrings and check whether palindrome or not and return substring.
+
+#  Time Complexity: O(n³)
+#  Space Complexity: O(n)
 class Solution(object):
     def longestPalindrome(self, s):
         longest = ""
