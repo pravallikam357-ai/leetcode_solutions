eba4fe9aeb85850594d285e1365e6719102452fb
pravallikam357-ai <pravallikam357@gmail.com>
2026-05-05 20:17:22 +0530

    Update ugly_number.py

--- ugly_number.py
+++ ugly_number.py
@@ -1,3 +1,24 @@
+# Problem: Ugly Number
+# Platform: LeetCode
+# Difficulty: Easy
+# Topic: Math
+# Language: Python
+
+# Problem Description:
+# An ugly number is a positive integer which does not have a prime factor other than 2, 3, and 5.
+
+#Given an integer n, return true if n is an ugly number..
+
+#  Approach:
+# An ugly number is a number whose only prime factors are 2, 3, and 5.
+
+#Your logic:
+
+#Keep dividing n by 2, 3, or 5 whenever possible
+#If at some point it’s not divisible by any of them - not ugly
+#If you reduce it to 1 - ugly number
+#  Time Complexity: O(log n)
+#  Space Complexity: O(1)
 class Solution(object):
     def isUgly(self, n):
         while n>1:
