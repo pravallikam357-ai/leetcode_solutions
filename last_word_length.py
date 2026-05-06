ef79d3713e0274c8b6cd2ef390c8e9a1cba69057
pravallikam357-ai <pravallikam357@gmail.com>
2026-05-06 21:39:49 +0530

    Update last_word_length.py

--- last_word_length.py
+++ last_word_length.py
@@ -1,3 +1,17 @@
+# Problem: Length of the last word in a given string
+# Platform: LeetCode
+# Difficulty: Easy
+# Topic: Strings
+# Language: Python
+
+#  Problem Description:
+# Find the length of the last word in the given string.
+
+#  Approach:
+# Use split method and split with spaces and find the length of last string.
+
+#  Time Complexity: O(n)
+#  Space Complexity: O(1)
 class Solution(object):
     def lengthOfLastWord(self, s):
         splits=s.split()
