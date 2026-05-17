99a5b3a1bc08ee25e5eaba9aaefcc7034567a8ef
pravallikam357-ai <pravallikam357@gmail.com>
2026-05-17 11:57:11 +0530

    Update longest_common_prefix.py

--- longest_common_prefix.py
+++ longest_common_prefix.py
@@ -1,3 +1,25 @@
+# Problem: Longest Common Prefix
+# Platform: LeetCode
+# Difficulty: Easy
+# Topic: Arrays,strings
+# Language: Python
+
+# Problem Description:
+# Find the longest common prefix in a given list of strings.
+
+#  Approach:
+# Take the first string as the reference string.
+#Traverse each character of the first string.
+#Compare that character with the character at the same index in all other strings
+
+#characters mismatch, or
+#index exceeds any string length
+
+#then return the prefix found till now.
+
+#If all characters match, return the entire first string.
+#  Time Complexity: O(n*m)
+#  Space Complexity: O(1)
 class Solution(object):
     def longestCommonPrefix(self, strs):
         for i in range(len(strs[0])):
