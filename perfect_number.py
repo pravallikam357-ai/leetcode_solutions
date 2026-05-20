488cd15fc9538474d9ab9a67eb92e14f5423df62
pravallikam357-ai <pravallikam357@gmail.com>
2026-05-20 12:01:38 +0530

    Add files via upload

---
+++ perfect_number.py
@@ -0,0 +1,20 @@
+import math
+
+class Solution(object):
+    def checkPerfectNumber(self, num):
+
+        if num <= 1:
+            return False
+
+        add = 1
+
+        for i in range(2, int(math.sqrt(num)) + 1):
+
+            if num % i == 0:
+
+                add += i
+
+                if i != num // i:
+                    add += num // i
+
+        return add == num
