397a80a75ea0df3284f7b0cbaff850eb668a1c1a
pravallikam357-ai <pravallikam357@gmail.com>
2026-05-22 11:17:03 +0530

    Update remove_trailing_zeroes_from_string.py

--- remove_trailing_zeroes_from_string.py
+++ remove_trailing_zeroes_from_string.py
@@ -1,3 +1,17 @@
+# Problem: Remove Trailing zeroes from a given string
+# Platform: LeetCode
+# Difficulty: Easy
+# Topic: Strings
+# Language: Python
+
+#  Problem Description:
+# Given a positive integer num represented as a string, return the integer num without trailing zeros as a string..
+
+#  Approach:
+# Take the string as integer and obtain the last digit if zero the delete otherwise return the same integer as a string.
+
+#  Time Complexity: O(n)
+#  Space Complexity: O(1)
 class Solution(object):
     def removeTrailingZeros(self, num):
         num=int(num)
