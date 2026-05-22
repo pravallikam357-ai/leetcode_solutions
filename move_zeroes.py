df14a384155c4e65028ce394218fbc91c9121f11
pravallikam357-ai <pravallikam357@gmail.com>
2026-05-22 11:12:10 +0530

    Update move_zeroes.py

--- move_zeroes.py
+++ move_zeroes.py
@@ -1,3 +1,17 @@
+# Problem: Move Zeroes
+# Platform: LeetCode
+# Difficulty: Easy
+# Topic: Arrays,Two pointers
+# Language: Python
+
+#  Problem Description:
+# Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+#  Approach:
+# Create two arrays for zeroes and nonzeroes and merge them to move all zeroes to the end.
+
+#  Time Complexity: O(n)
+#  Space Complexity: O(n)
 class Solution(object):
     def moveZeroes(self, nums):
         z = []
