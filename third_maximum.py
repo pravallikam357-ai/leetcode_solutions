48dcdca16c8cbe4c2979e62da9914a4868b64132
pravallikam357-ai <pravallikam357@gmail.com>
2026-05-23 19:40:04 +0530

    Update third_maximum.py

--- third_maximum.py
+++ third_maximum.py
@@ -1,3 +1,19 @@
+# Problem: Third Maximum Number
+# Platform: LeetCode
+# Difficulty: Easy
+# Topic: Arrays,Sorting
+# Language: Python
+
+# Problem Description:
+# Given an integer array nums, return the third distinct maximum number in this array. If the third maximum does not exist, return the maximum number.
+
+ 
+
+#  Approach:
+# Find the first two maximum numbers in the array and remove them and find the maximum again .
+
+#  Time Complexity: O(n)
+#  Space Complexity: O(n)
 class Solution(object):
     def thirdMax(self, nums):
         nums = list(set(nums))   # remove duplicates
