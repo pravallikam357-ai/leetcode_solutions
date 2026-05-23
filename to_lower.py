ca7ca012e4a5200f4cb766f2fba5cc7b10bc3485
pravallikam357-ai <pravallikam357@gmail.com>
2026-05-23 19:43:59 +0530

    Update to_lower.py

--- to_lower.py
+++ to_lower.py
@@ -1,3 +1,17 @@
+# Problem: To Lower
+# Platform: LeetCode
+# Difficulty: Easy
+# Topic: Strings
+# Language: Python
+
+#  Problem Description:
+# Convert the given string to lower case.
+
+#  Approach:
+# convert the given string to lower casee by using .lower() function.
+
+#  Time Complexity: O(n)
+#  Space Complexity: O(n)
 class Solution(object):
     def toLowerCase(self, s):
        return s.lower()
