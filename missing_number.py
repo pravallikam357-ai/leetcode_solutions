d48d6f673bf583764e85840a9890c33e93aff8e9
pravallikam357-ai <pravallikam357@gmail.com>
2026-05-24 12:54:56 +0530

    Update missing_number.py

--- missing_number.py
+++ missing_number.py
@@ -1,3 +1,17 @@
+# Problem: Missing Number
+# Platform: LeetCode
+# Difficulty: Easy
+# Topic: Arrays,Hash Table,Math,Sorting,Bit Manipulation,Binary search
+# Language: Python
+
+#  Problem Description:
+# Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+
+#  Approach:
+# Acces each item of the array upto the given length and return the missing value between the range.
+
+#  Time Complexity: O(n²)
+#  Space Complexity: O(1)
 class Solution(object):
     def missingNumber(self, nums):
         for i in range(len(nums)+1):
