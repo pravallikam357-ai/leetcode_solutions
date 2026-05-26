09496eba84187e724f83deafa5b524371fc1da0b
pravallikam357-ai <pravallikam357@gmail.com>
2026-05-26 19:07:01 +0530

    Update jewels_and_stones.py

--- jewels_and_stones.py
+++ jewels_and_stones.py
@@ -1,3 +1,17 @@
+# Problem: Jewels and Stones
+# Platform: LeetCode
+# Difficulty: Easy
+# Topic: Hash tables,Strings
+# Language: Python
+
+#  Problem Description:
+# You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
+
+#Letters are case sensitive, so "a" is considered a different type of stone from "A"
+#  Approach:
+# check wether given in stones are in jewels then increase the count and return the final count
+#  Time Complexity: O(n*m)
+#  Space Complexity: O(1)
 class Solution(object):
     def numJewelsInStones(self, jewels, stones):
         count=0
