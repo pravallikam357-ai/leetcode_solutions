34a64cfa20109e2358ff901c24fd6a4a1c39180e
pravallikam357-ai <pravallikam357@gmail.com>
2026-05-26 19:00:20 +0530

    Update buddy_strings.py

--- buddy_strings.py
+++ buddy_strings.py
@@ -1,3 +1,22 @@
+# Problem: Buddy Strings
+# Platform: LeetCode
+# Difficulty: Easy
+# Topic: Hash tables,Strings
+# Language: Python
+
+#  Problem Description:
+# Given two strings s and goal, return true if you can swap two letters in s so the result is equal to goal, otherwise, return false.
+
+#Swapping letters is defined as taking two indices i and j (0-indexed) such that i != j and swapping the characters at s[i] and s[j].
+
+#For example, swapping at indices 0 and 2 in "abcd" results in "cbad"
+#  Approach:
+# Traverse both strings and store indices where characters differ.
+#If strings are already equal, return True only when there is at least one duplicate character.
+#Otherwise, there must be exactly 2 mismatches, and swapping those characters in s should make it equal to goal..
+
+#  Time Complexity: O(n)
+#  Space Complexity: O(1)
 class Solution(object):
     def buddyStrings(self, s, goal):
 
