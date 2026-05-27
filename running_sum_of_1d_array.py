38ae9bbdd3598bebbaabd9cba1ae999e3e3493e4
pravallikam357-ai <pravallikam357@gmail.com>
2026-05-27 15:03:38 +0530

    Update running_sum_of_1d_array.py

--- running_sum_of_1d_array.py
+++ running_sum_of_1d_array.py
@@ -1,3 +1,18 @@
+# Problem: Running Sum of 1D Array
+# Platform: LeetCode
+# Difficulty: Easy
+# Topic: Arrays,Prefix Sum
+# Language: Python
+
+#  Problem Description:
+# Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+
+#Return the running sum of nums..
+
+#  Approach:
+#Access each element and add every element with the next one and return the final sum
+#  Time Complexity: O(n)
+#  Space Complexity: O(n)
 class Solution(object):
     def runningSum(self, nums):
         run=0
