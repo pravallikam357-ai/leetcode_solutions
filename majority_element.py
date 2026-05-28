53431354a24d864089bb2445c53d6c5d56a19930
pravallikam357-ai <pravallikam357@gmail.com>
2026-05-28 12:41:22 +0530

    Update majority_element.py

--- majority_element.py
+++ majority_element.py
@@ -1,3 +1,23 @@
+# Problem: Majority Element
+# Platform: LeetCode
+# Difficulty: Easy
+# Topic: Arrays,Hashtable,divide and conquer ,sorting,counting
+# Language: Python
+
+#  Problem Description:
+# Given an array nums of size n, return the majority element.
+
+#The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+ 
+
+#  Approach:
+#First, sort the array.
+#In a sorted array, the majority element (appearing more than n/2 times) will always occupy the middle position.
+#So return the element at index len(nums)//2.
+
+#  Time Complexity: O(n log n)
+#  Space Complexity: O(1)
 class Solution(object):
     def majorityElement(self, nums):
         nums.sort()
