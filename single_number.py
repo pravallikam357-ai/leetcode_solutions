e5402c850b7aad49595d4db98e1e97674752f14f
pravallikam357-ai <pravallikam357@gmail.com>
2026-05-28 12:36:33 +0530

    Update single_number.py

--- single_number.py
+++ single_number.py
@@ -1,3 +1,18 @@
+# Problem: Single Number
+# Platform: LeetCode
+# Difficulty: Easy
+# Topic: Arrays,Bit Manipulation
+# Language: Python
+
+#  Problem Description:
+# Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+#You must implement a solution with a linear runtime complexity and use only constant extra space..
+
+#  Approach:
+# Access each element and count the no.of occurences and return the number which has count 1
+#  Time Complexity: O(n²)
+#  Space Complexity: O(1)
 class Solution(object):
     def singleNumber(self, nums):
         for i in nums:
