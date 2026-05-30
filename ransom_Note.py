0c20ad14da8ffd820dc41e63edf1515e2f66238c
pravallikam357-ai <pravallikam357@gmail.com>
2026-05-30 16:22:10 +0530

    Update ransom_Note.py

--- ransom_Note.py
+++ ransom_Note.py
@@ -1,3 +1,26 @@
+# Problem: RansomNote
+# Platform: LeetCode
+# Difficulty: Easy
+# Topic: Hashtable,string,counting
+# Language: Python
+
+#  Problem Description:
+# Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
+
+#Each letter in magazine can only be used once in ransomNote.
+
+#  Approach:
+# The idea is to check whether every character in ransomNote appears in magazine at least as many times as needed.
+
+#Loop through each character ch in ransomNote.
+#Compare:
+#magazine.count(ch) → frequency of ch in magazine
+#ransomNote.count(ch) → frequency of ch needed
+#If the available count is less than the required count, return False.
+#If all characters satisfy the condition, return True.
+
+#  Time Complexity: O(n×(m+n))
+#  Space Complexity: O(1)
 class Solution(object):
     def canConstruct(self, ransomNote, magazine):
 
