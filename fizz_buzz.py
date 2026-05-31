81c9f9544bebc8fe2ca4db5dd79bda07d84e575b
pravallikam357-ai <pravallikam357@gmail.com>
2026-05-31 18:34:58 +0530

    Add files via upload

---
+++ fizz_buzz.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def fizzBuzz(self, n):
+        answers=[]
+        for i in range(1,n+1):
+            if i%3==0 and i%5==0:
+                answers.append("FizzBuzz")
+            elif i%3==0:
+                answers.append("Fizz")
+            elif  i%5==0:
+                answers.append("Buzz")
+            else:
+                answers.append(str(i))
+        return answers
+
+        
