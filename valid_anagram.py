62620ed274d2a0da1b4b922108247642995afe2b
pravallikam357-ai <pravallikam357@gmail.com>
2026-05-31 18:33:32 +0530

    Update valid_anagram.py

--- valid_anagram.py
+++ valid_anagram.py
@@ -1,3 +1,18 @@
+# Problem: Valid Anagram
+# Platform: LeetCode
+# Difficulty: Easy
+# Topic: Hashtable,strings,sorting
+# Language: Python
+
+#  Problem Description:
+# Given two strings s and t, return true if t is an anagram of s, and false otherwise
+#  Approach:
+#sorted(s) sorts all characters of s.
+#sorted(t) sorts all characters of t.
+#If the sorted character sequences are identical, the strings are anagrams; otherwise, they are not.
+
+#  Time Complexity: O(n log n)
+#  Space Complexity: O(n)
 class Solution(object):
     def isAnagram(self, s, t):
         return sorted(s)==sorted(t)
