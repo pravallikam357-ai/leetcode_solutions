ba16b0c3770c2cb2f595c4370e8f141cda0d7bfe
pravallikam357-ai <pravallikam357@gmail.com>
2026-05-31 18:37:12 +0530

    Update fizz_buzz.py

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -1,3 +1,27 @@
+# Problem: Fizz Buzz
+# Platform: LeetCode
+# Difficulty: Easy
+# Topic: Math,Strings,Simulation
+# Language: Python
+
+#  Problem Description:
+# Given an integer n, return a string array answer (1-indexed) where:
+
+#answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+#answer[i] == "Fizz" if i is divisible by 3.
+#answer[i] == "Buzz" if i is divisible by 5.
+#answer[i] == i (as a string) if none of the above conditions are true.
+ 
+#  Approach:
+#The goal is to generate a list from 1 to n:
+
+#If a number is divisible by 3 and 5, add "FizzBuzz".
+#If divisible by 3 only, add "Fizz".
+#If divisible by 5 only, add "Buzz".
+#Otherwise, add the number itself as a string..
+
+#  Time Complexity: O(n)
+#  Space Complexity: O(n)
 class Solution(object):
     def fizzBuzz(self, n):
         answers=[]
