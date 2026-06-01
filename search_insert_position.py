fc15e9bd6cc766f9d4df6fc41f09f131b7d3b286
pravallikam357-ai <pravallikam357@gmail.com>
2026-06-01 15:31:06 +0530

    Update search_insert_position.py

--- search_insert_position.py
+++ search_insert_position.py
@@ -1,3 +1,25 @@
+# Problem: Search insert position
+# Platform: LeetCode
+# Difficulty: Easy
+# Topic: Arrays,Binarysearch
+# Language: Python
+
+#  Problem Description:
+# Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+#You must write an algorithm with O(log n) runtime complexity.
+# Approach:
+# The array is already sorted.
+
+#Traverse the array from left to right.
+#For each element, check if it is greater than or equal to the target.
+#If found, return its index because:
+#If the target exists, this is its position.
+#If the target does not exist, this is where it should be inserted.
+#If no element is greater than or equal to the target, the target should be inserted at the end of the array, so return len(nums)
+
+#  Time Complexity: O(n)
+#  Space Complexity: O(1)
 class Solution(object):
     def searchInsert(self, nums, target):
         for i in range(len(nums)):
