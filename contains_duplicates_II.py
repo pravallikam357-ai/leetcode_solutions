25e5ae4870fb81815fbab518c008eebca2f66060
pravallikam357-ai <pravallikam357@gmail.com>
2026-06-02 11:41:50 +0530

    Update contains_duplicates_II.py

--- contains_duplicates_II.py
+++ contains_duplicates_II.py
@@ -1,3 +1,20 @@
+# Problem: Contains Duplicates II
+# Platform: LeetCode
+# Difficulty: Easy
+# Topic: Arrays,Hashtable,Sliding window
+# Language: Python
+
+#  Problem Description:
+# Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
+#  Approach:
+# Traverse the array from left to right.
+#For each element:
+#If it was seen before, check the index difference.
+#If the difference ≤ k, return True.
+#Otherwise, update its latest index..
+
+#  Time Complexity: O(n)
+#  Space Complexity: O(n)
 class Solution(object):
     def containsNearbyDuplicate(self, nums, k):
         d = {}
