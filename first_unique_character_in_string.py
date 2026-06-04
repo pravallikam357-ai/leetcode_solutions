3ec894f2bc552db1b1c75350c09071d9ddcf6dca
pravallikam357-ai <pravallikam357@gmail.com>
2026-06-04 11:55:47 +0530

    Update first_unique_character_in_string.py

--- first_unique_character_in_string.py
+++ first_unique_character_in_string.py
@@ -1,3 +1,18 @@
+# Problem: First Unique character in a string
+# Platform: LeetCode
+# Difficulty: Easy
+# Topic: Strings,queue,Hashtable,counting
+# Language: Python
+
+#  Problem Description:
+# Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
+
+#  Approach:
+# Traverse the string and store the count of every character in d.
+#Traverse the string again using indices.
+#The first character whose frequency is 1 is the first non-repeating character, so return its index.
+#  Time Complexity: O(n)
+#  Space Complexity: O(1)
 class Solution(object):
     def firstUniqChar(self, s):
         d = {}
