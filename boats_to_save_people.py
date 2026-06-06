ec63bd9ba85817915fd4f37da4f7d9e93b5f26aa
pravallikam357-ai <pravallikam357@gmail.com>
2026-06-06 10:22:17 +0530

    Add files via upload

---
+++ boats_to_save_people.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def numRescueBoats(self, people, limit):
+        people.sort()
+        
+        i = 0
+        j = len(people) - 1
+        boats = 0
+        
+        while i <= j:
+            if people[i] + people[j] <= limit:
+                i += 1   
+            j -= 1       
+            boats += 1
+        
+        return boats
