4d9842796fd386bf8f739df37348f108788b434d
pravallikam357-ai <pravallikam357@gmail.com>
2026-06-07 15:32:04 +0530

    Update valid_binary_strings_with_cost_limit.py

--- valid_binary_strings_with_cost_limit.py
+++ valid_binary_strings_with_cost_limit.py
@@ -1,3 +1,29 @@
+# Problem: Valid Binary Strings with cost limit
+# Platform: LeetCode
+# Difficulty: Medium
+# Topic: Arrays
+# Language: Python
+
+#  Problem Description:
+# You are given two integers n and k.
+
+#The cost of a binary string s is defined as the sum of all indices i (0-based) such that s[i] == '1'.
+
+#A binary string is considered valid if:
+#It does not contain two consecutive '1' characters.
+#Its cost is less than or equal to k.
+#Return a list of all valid binary strings of length n in any order.
+#  Approach:
+# This solution uses backtracking (DFS) to generate all binary strings of length n.
+
+#Parameters of dfs:
+
+#i → current index being filled.
+#prev → previous character placed.
+#cost → accumulated cost so far.
+#cur → current string being built.
+#  Time Complexity: O(Fn+2​)≈O(1.618n)
+#  Space Complexity: O(n)
 class Solution(object):
     def generateValidStrings(self, n, k):
         ans = []
