aae914fed348b5a43ec1f1ee59d721c739063123
pravallikam357-ai <pravallikam357@gmail.com>
2026-06-25 18:58:34 +0530

    Update squares_of_sorted_array.py

--- squares_of_sorted_array.py
+++ squares_of_sorted_array.py
@@ -1,3 +1,21 @@
+# Problem: Squares of sorted array
+# Platform: LeetCode
+# Difficulty: Easy
+# Topic: Arrays,Two pointer,Sorting
+# Language: Python
+
+#  Problem Description:
+# Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+#  Approach:
+# Create an empty list l.
+#Traverse the given array:
+#Take each element.
+#Find its square:
+#Store the square in the list.
+#After creating all squared values, sort the list:
+#Return the sorted squared values.
+#  Time Complexity: O(n log n)
+#  Space Complexity: O(n)
 class Solution(object):
     def sortedSquares(self, nums):
         l=[]
