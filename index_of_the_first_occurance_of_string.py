706b63cba6c95fa95924a32ff7bef79054a330d3
pravallikam357-ai <pravallikam357@gmail.com>
2026-07-04 15:28:41 +0530

    Update index_of_the_first_occurance_of_string.py

--- index_of_the_first_occurance_of_string.py
+++ index_of_the_first_occurance_of_string.py
@@ -1,3 +1,25 @@
+# Problem: Find the index of the first occurance of the string
+# Platform: LeetCode
+# Difficulty: Easy
+# Topic: Two pointers,String,String matching
+# Language: Python
+
+#  Problem Description:
+# Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+#  Approach:
+# Find the lengths of haystack (n) and needle (m).
+#If needle is longer than haystack, it cannot be present, so return -1.
+#Traverse every possible starting index from 0 to n - m.
+#For each starting index i:
+#Compare the characters of needle with the corresponding characters in haystack.
+#Continue comparing until:
+#all characters match (j == m), or
+#a mismatch occurs.
+#If all characters match, return the starting index i.
+#If no match is found after checking every position, return -1..
+
+#  Time Complexity: O(n * m)
+#  Space Complexity: O(1)
 class Solution(object):
     def strStr(self, haystack, needle):
         n=len(haystack)
