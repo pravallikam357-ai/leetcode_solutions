bda4a69e8b0cf7f6fd0c617426751a382ae54f5e
pravallikam357-ai <pravallikam357@gmail.com>
2026-07-08 17:18:48 +0530

    Update single_numberIII.py

--- single_numberIII.py
+++ single_numberIII.py
@@ -1,3 +1,22 @@
+# Problem: Single NumberIII
+# Platform: LeetCode
+# Difficulty: Medium
+# Topic: Arrays,Bit Manipulation
+# Language: Python
+
+#  Problem Description:
+# Given an integer array nums, in which exactly two elements appear only once and all the other elements appear exactly twice. Find the two elements that appear only once. You can return the answer in any order.
+
+#You must write an algorithm that runs in linear runtime complexity and uses only constant extra space.
+#  Approach:
+# Create an empty list a.
+#Traverse each element i in nums.
+#Count how many times i appears using nums.count(i).
+#If the count is 1, append it to a.
+#Return a.
+
+#  Time Complexity: O(n)
+#  Space Complexity: O(1)
 class Solution(object):
     def singleNumber(self, nums):
         a=[]
