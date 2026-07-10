e73b35b52da8a58ad19886196d38918f67eef0d2
pravallikam357-ai <pravallikam357@gmail.com>
2026-07-10 11:08:46 +0530

    Update maximum_product_of_three_numbers.py

--- maximum_product_of_three_numbers.py
+++ maximum_product_of_three_numbers.py
@@ -1,3 +1,21 @@
+# Problem: Maximum of three numbers
+# Platform: LeetCode
+# Difficulty: Easy
+# Topic: Arrays,Math,Sorting
+# Language: Python
+
+#  Problem Description:
+# Given an integer array nums, find three numbers whose product is maximum and return the maximum product.
+
+#  Approach:
+# After sorting the array, there are only two possible candidates for the maximum product:
+#Product of the three largest numbers
+#Product of the two smallest numbers and the largest number
+#If the first two numbers are negative, their product becomes positive.
+#Multiplying this positive value by the largest positive number may produce a larger product than using the three largest numbers.
+
+#  Time Complexity: O(n log n)
+#  Space Complexity: O(1)
 class Solution(object):
     def maximumProduct(self, nums):
         nums.sort()
